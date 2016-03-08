fccba94b94ea1e1f845ecbb3e34d5ecb03105c0a
incredible <luckyckgp007@gmail.com>
2016-03-08 05:32:03 -0500

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# vuex-router-sync
+
+> Effortlessly keep vue-router and vuex store in sync.
+
+### Usage
+
+``` js
+import { sync } from 'vuex-router-sync'
+import store from './vuex/store' // vuex store instance
+import router from './router' // vue-router instance
+
+sync(store, router) // done.
+
+// bootstrap your app...
+```
+
+### How does it work?
+
+- It adds a `route` module into the store, which contains the state representing the current route.
+
+- When the router navigates to a new route, the store's state is updated.
+
+- When the store's `route` state is updated, it notifies the router to navigate to the corresponding path. This enables time-traveling between routes in `vue-devtools`.
---
+++ index.js
@@ -0,0 +1,64 @@
+exports.sync = function (store, router) {
+  patchStore(store)
+  store.router = router
+
+  var isTimeTraveling = false
+  var currentPath
+
+  // sync router on store change
+  store.watch(
+    function (state) {
+      return state.route
+    },
+    function (route) {
+      if (route.path === currentPath) {
+        return
+      }
+      isTimeTraveling = true
+      currentPath = route.path
+      router.go(route.path)
+    },
+    { deep: true, sync: true }
+  )
+
+  // sync store on router navigation
+  router.afterEach(function (transition) {
+    if (isTimeTraveling) {
+      isTimeTraveling = false
+      return
+    }
+    var to = transition.to
+    currentPath = to.path
+    store.dispatch('router/ROUTE_CHANGED', {
+      path: to.path,
+      query: to.query,
+      params: to.params
+    })
+  })
+}
+
+function patchStore (store) {
+  // add state
+  var set = store._vm.constructor.parsers.path.setPath
+  store._dispatching = true
+  set(store._vm._data, 'route', {
+    path: '',
+    query: null,
+    params: null
+  })
+  store._dispatching = false
+  // add mutations
+  store.hotUpdate({
+    modules: {
+      route: {
+        mutations: {
+          'router/ROUTE_CHANGED': function (state, to) {
+            state.path = to.path
+            state.query = to.query
+            state.params = to.params
+          }
+        }
+      }
+    }
+  })
+}
