6e0353f008299a5ee2bac0215b600a5672fbb26d
incredible <luckyckgp007@gmail.com>
2016-08-05 15:45:33 -0400

    compat with latest rc of vuex/vue-router

--- index.js
+++ index.js
@@ -1,74 +1,44 @@
 exports.sync = function (store, router) {
-  patchStore(store)
-  store.router = router
+  store.registerModule('route', {
+    state: {},
+    mutations: {
+      'router/ROUTE_CHANGED': function (state, to) {
+        store.state.route = Object.freeze({
+          name: to.name,
+          path: to.path,
+          hash: to.hash,
+          query: to.query,
+          params: to.params,
+          fullPath: to.fullPath
+        })
+      }
+    }
+  })
 
-  var commit = store.commit || store.dispatch
   var isTimeTraveling = false
   var currentPath
 
   // sync router on store change
   store.watch(
-    function (state) {
-      return state.route
-    },
+    function (state) { return state.route },
     function (route) {
-      if (route.path === currentPath) {
+      if (route.fullPath === currentPath) {
         return
       }
       isTimeTraveling = true
-      currentPath = route.path
-      router.go(route.path)
+      currentPath = route.fullPath
+      router.push(route)
     },
-    { deep: true, sync: true }
+    { sync: true }
   )
 
   // sync store on router navigation
-  router.afterEach(function (transition) {
+  router.afterEach(function (to) {
     if (isTimeTraveling) {
       isTimeTraveling = false
       return
     }
-    var to = transition.to
-    currentPath = to.path
-    commit('router/ROUTE_CHANGED', to)
+    currentPath = to.fullPath
+    store.commit('router/ROUTE_CHANGED', to)
   })
-}
-
-function applyMutationState(store, state) {
-  // support above 2.0
-  if (store.hasOwnProperty('_committing')) {
-    return store._committing = state
-  }
-  return store._dispatching = state
-}
-
-function patchStore (store) {
-  // add state
-  var set = store._vm.constructor.set
-  applyMutationState(store, true);
-  set(store.state, 'route', {
-    path: '',
-    query: null,
-    params: null
-  })
-  applyMutationState(store, false);
-
-  var routeModule = {
-    mutations: {
-      'router/ROUTE_CHANGED': function (state, to) {
-        store.state.route = to
-      }
-    }
-  }
-
-  // add module
-  if (store.module) {
-    store.module('route', routeModule)
-  } else {
-    store.hotUpdate({
-      modules: {
-        route: routeModule
-      }
-    })
-  }
 }
