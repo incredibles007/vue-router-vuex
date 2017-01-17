34409231831a7fff4e81f5ae129b7edb873aa94a
Ryu Tamaki <ryu.mochi@gmail.com>
2017-01-17 23:12:34 +0900

    Return void from sync() (#41)

--- index.d.ts
+++ index.d.ts
@@ -5,4 +5,4 @@ interface SyncOptions {
   moduleName: string;
 }
 
-export declare function sync(store: Store<any>, router: VueRouter, options?: SyncOptions);
+export declare function sync(store: Store<any>, router: VueRouter, options?: SyncOptions): void;
