980b28c34ac71545c96c9f2bcbc29b68bd6e8a14
incredible <luckyckgp007@gmail.com>
2017-09-01 11:49:15 -0400

    update types

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,12 @@
-import {Store} from 'vuex';
+import { Store } from 'vuex';
 import VueRouter = require('vue-router');
 
 interface SyncOptions {
   moduleName: string;
 }
 
-export declare function sync(store: Store<any>, router: VueRouter, options?: SyncOptions): void;
+export declare function sync(
+  store: Store<any>,
+  router: VueRouter,
+  options?: SyncOptions
+): Function;
